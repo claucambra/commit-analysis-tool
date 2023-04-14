7013cb78b94f50c46f94be6a2c1f6c35b83b47db
Claudio Cambra <developer@claudiocambra.com>
2023-04-15 01:17:07 +0800

    Add git log pretty format parameter string
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- internal/git/commitformat.go
+++ internal/git/commitformat.go
@@ -1,3 +1,16 @@
 package git
 
+import (
+	"fmt"
+)
+
 const PrettyFormatSeparator = "__SEPARATOR__"
+
+var PrettyFormat = fmt.Sprintf("%%H%s%%aD%s%%aN%s%%aE%s%%cD%s%%cN%s%%cE",
+	PrettyFormatSeparator,
+	PrettyFormatSeparator,
+	PrettyFormatSeparator,
+	PrettyFormatSeparator,
+	PrettyFormatSeparator,
+	PrettyFormatSeparator)
+
