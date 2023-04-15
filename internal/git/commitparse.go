07cb1453d951f4e9282a472b572d48b405dccc9f
Claudio Cambra <developer@claudiocambra.com>
2023-04-16 00:54:41 +0800

    Add function to parse commit log output per our format
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- internal/git/commitparse.go
+++ internal/git/commitparse.go
@@ -15,6 +15,42 @@ var insertionsRegex = regexp.MustCompile("([0-9]+) insertions?")
 var deletionsRegex = regexp.MustCompile("([0-9]+) deletions?")
 
 /**
+ * Parse a commit according to the standard format defined in commitformat.go
+ *
+ * Example commit:
+
+	1c915e7dd147d4b060c2c241bb966d6f6c6ecde9__SEPARATOR__Sat, 8 Apr 2023 17:47:43 +0800__SEPARATOR__Claudio Cambra__SEPARATOR__developer@claudiocambra.com__SEPARATOR__Wed, 12 Apr 2023 23:21:43 +0000__SEPARATOR__Jean-Baptiste Kempf__SEPARATOR__jb@videolan.org
+	modules/gui/macosx/library/VLCLibraryWindow.h                            |  6 +++---
+	modules/gui/macosx/library/VLCLibraryWindowPersistentPreferences.h       | 22 +++++++++-------------
+	modules/gui/macosx/library/VLCLibraryWindowPersistentPreferences.m       | 30 +++++++++++++++---------------
+	modules/gui/macosx/library/audio-library/VLCLibraryAudioViewController.m |  4 ++--
+	modules/gui/macosx/library/media-source/VLCMediaSourceBaseDataSource.m   |  4 ++--
+	modules/gui/macosx/library/video-library/VLCLibraryVideoViewController.m |  2 +-
+	6 files changed, 32 insertions(+), 36 deletions(-)
+
+ **/
+
+func ParseCommit(rawCommit string) (*CommitData, error) {
+	commitLogLines := strings.Split(rawCommit, "\n")
+	prettyLogLine := commitLogLines[0]
+
+	changesLogLine := commitLogLines[len(commitLogLines)-1]
+
+	insertions, _ := parseChangesLine(changesLogLine, insertionsRegex)
+	deletions, _ := parseChangesLine(changesLogLine, deletionsRegex)
+
+	commit, err := parsePrettyLogLine(prettyLogLine)
+	if err != nil {
+		return nil, err
+	}
+
+	commit.numInsertions = insertions
+	commit.numDeletions = deletions
+
+	return commit, nil
+}
+
+/**
  * Convenience function to get a specific number of changes in the changes line (i.e. 320 insertions).
  */
 func parseChangesLine(changesLogLine string, specificChangesRegex *regexp.Regexp) (int, error) {
