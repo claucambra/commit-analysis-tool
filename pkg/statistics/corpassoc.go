da31c4b79c200af1193f313cd871cb567cc78c33
Claudio Cambra <developer@claudiocambra.com>
2023-04-18 00:46:07 +0800

    Add corporate authors report factory function
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/corpassoc.go
+++ pkg/statistics/corpassoc.go
@@ -1,6 +1,9 @@
 package statistics
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/claucambra/commit-analysis-tool/internal/git"
 )
 
@@ -12,3 +15,50 @@ type CorpAuthorsReport struct {
 	DomainCountMap     map[string]int
 }
 
+func NewCorpAuthorsReport(commits []*git.CommitData, corporateEmailDomains map[string]bool) *CorpAuthorsReport {
+	report := &CorpAuthorsReport{
+		Commits:            commits,
+		TotalAuthors:       0,
+		NumCorpAuthors:     0,
+		CorpAuthorsPercent: 0,
+		DomainCountMap:     make(map[string]int),
+	}
+
+	if len(commits) == 0 {
+		return report
+	}
+
+	authorsSet := make(map[string]bool)
+	domainCounts := make(map[string]int)
+
+	for _, commit := range commits {
+		authorString := commit.AuthorEmail
+		if authorString == "" {
+			authorString = commit.AuthorName
+		}
+
+		if authorsSet[authorString] { // Already counted, skip
+			continue
+		} else if authorString != "" {
+			authorsSet[authorString] = true
+			report.TotalAuthors += 1
+		}
+
+		splitAuthorEmail := strings.Split(commit.AuthorEmail, "@")
+
+		if len(splitAuthorEmail) < 2 {
+			domainCounts["unknown"] += 1
+			continue
+		}
+
+		emailDomain := splitAuthorEmail[1]
+		report.DomainCountMap[emailDomain] += 1
+
+		if corporateEmailDomains[emailDomain] {
+			report.NumCorpAuthors += 1
+		}
+	}
+
+	report.CorpAuthorsPercent = (float32(report.NumCorpAuthors) / float32(report.TotalAuthors)) * 100
+	return report
+}
