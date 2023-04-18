d24b81b80377c3ae8b043b81816833c35fafbb67
Claudio Cambra <developer@claudiocambra.com>
2023-04-19 01:38:26 +0800

    Use convenience func in sqlite_test
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- internal/db/sqlite_test.go
+++ internal/db/sqlite_test.go
@@ -11,53 +11,9 @@ import (
 	"github.com/claucambra/commit-analysis-tool/pkg/logread"
 )
 
-const testDbFileName = "test.db"
-const testDirName = "sqlite_test"
-
-var sqlb = new(SQLiteBackend)
-
-func setupTestDB() (string, error) {
-	testDir, err := os.MkdirTemp("", testDirName)
-	if err != nil {
-		return "", err
-	}
-
-	testDbPath := filepath.Join(testDir, testDbFileName)
-
-	fmt.Printf("Setting up test database at %s\n", testDir)
-	err = sqlb.Open(testDbPath)
-	if err != nil {
-		log.Fatalf("Could not open database: %s", err)
-		return "", err
-	}
-
-	err = sqlb.Setup()
-	if err != nil {
-		log.Fatalf("Could not setup database: %s", err)
-		return "", err
-	}
-
-	return testDir, err
-}
-
-func cleanup(path string) {
-	os.RemoveAll(path)
-}
-
-func TestSqliteDbSetup(t *testing.T) {
-	testPath, err := setupTestDB()
-	if err != nil {
-		t.Fatalf("Failed to setup database: %s", err)
-	}
-
-	cleanup := func() { cleanup(testPath) }
-	t.Cleanup(cleanup)
-
-}
-
 func TestSqliteDbAddCommit(t *testing.T) {
-	testPath, _ := setupTestDB()
-	cleanup := func() { cleanup(testPath) }
+	sqlb := InitTestDB(t)
+	cleanup := func() { CleanupTestDB(sqlb) }
 	t.Cleanup(cleanup)
 
 	testCommitLogBytes, err := os.ReadFile("../../test/data/log.txt")
