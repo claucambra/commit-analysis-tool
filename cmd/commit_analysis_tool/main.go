6cc0fd64589e4d26a88e7d76d57bbd9660dad7de
Claudio Cambra <developer@claudiocambra.com>
2023-04-19 03:57:41 +0800

    Add basic printing of domain group report in main.go
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- cmd/commit_analysis_tool/main.go
+++ cmd/commit_analysis_tool/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/claucambra/commit-analysis-tool/internal/db"
 	"github.com/claucambra/commit-analysis-tool/pkg/logread"
+	"github.com/claucambra/commit-analysis-tool/pkg/statistics/authorgroups"
 )
 
 func main() {
 	var (
-		ingestDbPath = flag.String("ingest-db-path", "", "path to database file")
-		readDbPath   = flag.String("read-db-path", "", "path to database file")
-		repoPath     = flag.String("repo-path", "", "path to git repository")
-		//domainGroupsFile = flag.String("domain-groups-file", "", "file containing email domain groups")
+		ingestDbPath         = flag.String("ingest-db-path", "", "path to database file")
+		readDbPath           = flag.String("read-db-path", "", "path to database file")
+		repoPath             = flag.String("repo-path", "", "path to git repository")
+		domainGroupsFilePath = flag.String("domain-groups-file-path", "", "file containing email domain groups")
 	)
 
 	flag.Parse()
@@ -32,6 +35,8 @@ func main() {
 
 	if *ingestDbPath != "" {
 		ingestRepoCommits(*ingestDbPath, *repoPath)
+	} else if *readDbPath != "" {
+		printDomainGroups(*readDbPath, *domainGroupsFilePath)
 	}
 }
 
@@ -57,5 +62,43 @@ func ingestRepoCommits(ingestDbPath string, repoPath string) {
 
 	sqlb.AddCommits(commits)
 	fmt.Printf("Done!\n")
+	sqlb.Close()
+	os.Exit(0)
+}
+
+func printDomainGroups(readDbPath string, domainGroupsFilePath string) {
+	if domainGroupsFilePath == "" {
+		log.Fatalf("Cannot create author domain group report without domain groups. Quitting.")
+		os.Exit(0)
+	}
+
+	sqlb := new(db.SQLiteBackend)
+	err := sqlb.Open(readDbPath)
+	if err != nil {
+		log.Fatalf("Error opening sqlite database, received error: %s", err)
+		os.Exit(0)
+	}
+
+	groupsJsonBytes, err := ioutil.ReadFile(domainGroupsFilePath)
+	if err != nil {
+		log.Fatalf("Error opening domain groups json file: %s", err)
+		os.Exit(0)
+	}
+
+	var groups map[string][]string
+	err = json.Unmarshal(groupsJsonBytes, &groups)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+		os.Exit(0)
+	}
+
+	report := authorgroups.NewDomainGroupsReport(groups)
+	report.Generate(sqlb)
+
+	for groupName, _ := range groups {
+		fmt.Printf("%+v", report.GroupData(groupName))
+	}
+
+	sqlb.Close()
 	os.Exit(0)
 }
