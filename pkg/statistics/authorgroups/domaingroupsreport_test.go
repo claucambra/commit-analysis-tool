e55f32b900cb0411544df8d9313770c2542a6b03
Claudio Cambra <developer@claudiocambra.com>
2023-04-19 02:36:42 +0800

    Clean up domaingroupsreport test
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/authorgroups/domaingroupsreport_test.go
+++ pkg/statistics/authorgroups/domaingroupsreport_test.go
@@ -1,44 +1,25 @@
 package authorgroups
 
 import (
-	"os"
 	"testing"
 
 	"github.com/claucambra/commit-analysis-tool/internal/db"
-	"github.com/claucambra/commit-analysis-tool/pkg/logread"
 )
 
 const testNumAuthors = 31
-const testGrouplessName = "unknown"
-const testNumGrouplessAuthors = 26
 const testNumGroupAuthors = 5
 const testGroupName = "VideoLAN"
 const testGroupDomain = "videolan.org"
 
-var testGrouplessAuthorsPercent = (float32(testNumGrouplessAuthors) / float32(testNumAuthors)) * 100
 var testGroupAuthorsPercent = (float32(testNumGroupAuthors) / float32(testNumAuthors)) * 100
 var testCommitsFile = "../../../test/data/log.txt"
-var testCommitsBytes, readFileErr = os.ReadFile(testCommitsFile)
-var testCommitsString = string(testCommitsBytes)
-var testCommits, readCommitsErr = logread.ParseCommitLog(testCommitsString)
 
 var testEmailGroups = map[string][]string{
 	testGroupName: {testGroupDomain},
 }
 
-func TestCommitsFile(t *testing.T) {
-	db.TestLogFilePath = "../../../test/data/log.txt"
-	if readFileErr != nil {
-		t.Fatalf("Received error on test data read: %s", readFileErr)
-	}
-
-	if readCommitsErr != nil {
-		t.Fatalf("Received error on reading commits: %s", readCommitsErr)
-	}
-}
-
 func TestNewDomainGroupsReport(t *testing.T) {
-	db.TestLogFilePath = "../../../test/data/log.txt"
+	db.TestLogFilePath = testCommitsFile
 	sqlb := db.InitTestDB(t)
 	cleanup := func() { db.CleanupTestDB(sqlb) }
 	t.Cleanup(cleanup)
