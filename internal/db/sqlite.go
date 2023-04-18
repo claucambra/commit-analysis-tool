14d3edcb03a337d0a9efa1f06d71ef1d2ed93996
Claudio Cambra <developer@claudiocambra.com>
2023-04-18 23:12:51 +0800

    Add table setup to sqlite
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -36,3 +36,34 @@ func (sqlb *SQLiteBackend) Close() error {
 
 	return err
 }
+
+func (sqlb *SQLiteBackend) Setup() error {
+	stmt := `CREATE TABLE IF NOT EXISTS commits (
+			id TEXT PRIMARY KEY ON CONFLICT REPLACE,
+			repo_name TEXT NOT NULL,
+			author_name TEXT,
+			author_email TEXT,
+			author_time INT,
+			committer_name TEXT,
+			committer_email TEXT,
+			committer_time INT,
+			num_insertions INT,
+			num_deletions INT,
+			num_files_changed INT);
+		CREATE INDEX IF NOT EXISTS index_repo_name ON commits (repo_name);
+		CREATE INDEX IF NOT EXISTS index_author_name ON commits (author_name);
+		CREATE INDEX IF NOT EXISTS index_author_email ON commits (author_email);
+		CREATE INDEX IF NOT EXISTS index_author_time ON commits (author_time);
+		CREATE INDEX IF NOT EXISTS index_committer_name ON commits (committer_name);
+		CREATE INDEX IF NOT EXISTS index_committer_email ON commits (committer_email);
+		CREATE INDEX IF NOT EXISTS index_committer_time ON commits (committer_time);`
+
+	_, err := sqlb.db.Exec(stmt)
+	if err != nil {
+		fmt.Printf("Setup failed, received error during table creation: %s", err)
+		return err
+	}
+
+	return nil
+}
+
