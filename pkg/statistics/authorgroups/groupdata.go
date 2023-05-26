b238506138e318883751a033cbb7c13d19ba29b6
Claudio Cambra <developer@claudiocambra.com>
2023-05-26 15:53:07 +0800

    Add convenience NewGroupData function
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/authorgroups/groupdata.go
+++ pkg/statistics/authorgroups/groupdata.go
@@ -11,3 +11,16 @@ type GroupData struct {
 	InsertionsPercent float32
 	DeletionsPercent  float32
 }
+
+func NewGroupData(report *DomainGroupsReport, groupName string, groupAuthors int, groupInsertions int, groupDeletions int) *GroupData {
+	groupData := new(GroupData)
+	groupData.GroupName = groupName
+	groupData.NumAuthors = groupAuthors
+	groupData.NumInsertions = groupInsertions
+	groupData.NumDeletions = groupDeletions
+	groupData.AuthorsPercent = (float32(groupAuthors) / float32(report.TotalAuthors)) * 100
+	groupData.InsertionsPercent = (float32(groupInsertions) / float32(report.TotalInsertions)) * 100
+	groupData.DeletionsPercent = (float32(groupDeletions) / float32(report.TotalDeletions)) * 100
+
+	return groupData
+}
