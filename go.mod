47c15642577dd0c60003d1a5a69a1da94ba288c7
Claudio Cambra <developer@claudiocambra.com>
2023-06-01 21:18:58 +0800

    Provide insert and delete correlations between corporate group and community group in corporate report
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/claucambra/commit-analysis-tool
 
 go 1.19
 
-require github.com/mattn/go-sqlite3 v1.14.16 // indirect
+require github.com/mattn/go-sqlite3 v1.14.16
+
+require gonum.org/v1/gonum v0.13.0 // indirect
--- pkg/statistics/authorgroups/corporatereport.go
+++ pkg/statistics/authorgroups/corporatereport.go
@@ -2,11 +2,15 @@ package authorgroups
 
 import (
 	"github.com/claucambra/commit-analysis-tool/internal/db"
+	"gonum.org/v1/gonum/stat"
 )
 
 type CorporateReport struct {
 	CorporateGroup *GroupData
 	CommunityGroup *GroupData
+
+	InsertionsCorrel float64
+	DeletionsCorrel  float64
 
 	domainGroupsReport *DomainGroupsReport
 }
@@ -22,9 +26,40 @@ func NewCorporateReport(groupsOfDomains map[string][]string, sqlb *db.SQLiteBack
 	corpGroup := domainGroupsReport.GroupData(corporateGroupName)
 	commGroup := domainGroupsReport.UnknownGroupData()
 
+	yearsInBothGroups := []int{}
+	for year := range corpGroup.YearlyLineChanges {
+		_, ok := commGroup.YearlyLineChanges[year]
+		if ok {
+			yearsInBothGroups = append(yearsInBothGroups, year)
+		}
+	}
+
+	corpYearlyInsertionsArr, corpYearlyDeletionsArr := corpGroup.YearlyLineChanges.SeparatedChangeArrays(yearsInBothGroups)
+	commYearlyInsertionsArr, commYearlyDeletionsArr := commGroup.YearlyLineChanges.SeparatedChangeArrays(yearsInBothGroups)
+
+	floatCorpInserts := make([]float64, len(yearsInBothGroups))
+	floatCorpDeletes := make([]float64, len(yearsInBothGroups))
+	floatCommInserts := make([]float64, len(yearsInBothGroups))
+	floatCommDeletes := make([]float64, len(yearsInBothGroups))
+
+	for i := 0; i < len(corpGroup.YearlyLineChanges); i++ {
+		floatCorpInserts[i] = float64(corpYearlyInsertionsArr[i])
+		floatCorpDeletes[i] = float64(corpYearlyDeletionsArr[i])
+	}
+	for i := 0; i < len(corpGroup.YearlyLineChanges); i++ {
+		floatCommInserts[i] = float64(commYearlyInsertionsArr[i])
+		floatCommDeletes[i] = float64(commYearlyDeletionsArr[i])
+	}
+
+	insertionsCorrel := stat.Correlation(floatCorpInserts, floatCommInserts, nil)
+	deletionsCorrel := stat.Correlation(floatCorpDeletes, floatCommDeletes, nil)
+
 	return &CorporateReport{
 		CorporateGroup: corpGroup,
 		CommunityGroup: commGroup,
+
+		InsertionsCorrel: insertionsCorrel,
+		DeletionsCorrel:  deletionsCorrel,
 
 		domainGroupsReport: domainGroupsReport,
 	}
