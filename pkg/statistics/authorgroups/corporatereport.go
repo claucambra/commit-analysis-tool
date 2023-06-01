7098c9ee23ac0d08338d5fe380e10f23abc6d58d
Claudio Cambra <developer@claudiocambra.com>
2023-06-01 22:04:52 +0800

    Extract group correlation calculation to GroupData
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/authorgroups/corporatereport.go
+++ pkg/statistics/authorgroups/corporatereport.go
@@ -1,9 +1,6 @@
 package authorgroups
 
-import (
-	"github.com/claucambra/commit-analysis-tool/internal/db"
-	"gonum.org/v1/gonum/stat"
-)
+import "github.com/claucambra/commit-analysis-tool/internal/db"
 
 type CorporateReport struct {
 	CorporateGroup *GroupData
@@ -26,33 +23,7 @@ func NewCorporateReport(groupsOfDomains map[string][]string, sqlb *db.SQLiteBack
 	corpGroup := domainGroupsReport.GroupData(corporateGroupName)
 	commGroup := domainGroupsReport.UnknownGroupData()
 
-	yearsInBothGroups := []int{}
-	for year := range corpGroup.YearlyLineChanges {
-		_, ok := commGroup.YearlyLineChanges[year]
-		if ok {
-			yearsInBothGroups = append(yearsInBothGroups, year)
-		}
-	}
-
-	corpYearlyInsertionsArr, corpYearlyDeletionsArr := corpGroup.YearlyLineChanges.SeparatedChangeArrays(yearsInBothGroups)
-	commYearlyInsertionsArr, commYearlyDeletionsArr := commGroup.YearlyLineChanges.SeparatedChangeArrays(yearsInBothGroups)
-
-	floatCorpInserts := make([]float64, len(yearsInBothGroups))
-	floatCorpDeletes := make([]float64, len(yearsInBothGroups))
-	floatCommInserts := make([]float64, len(yearsInBothGroups))
-	floatCommDeletes := make([]float64, len(yearsInBothGroups))
-
-	for i := 0; i < len(corpGroup.YearlyLineChanges); i++ {
-		floatCorpInserts[i] = float64(corpYearlyInsertionsArr[i])
-		floatCorpDeletes[i] = float64(corpYearlyDeletionsArr[i])
-	}
-	for i := 0; i < len(corpGroup.YearlyLineChanges); i++ {
-		floatCommInserts[i] = float64(commYearlyInsertionsArr[i])
-		floatCommDeletes[i] = float64(commYearlyDeletionsArr[i])
-	}
-
-	insertionsCorrel := stat.Correlation(floatCorpInserts, floatCommInserts, nil)
-	deletionsCorrel := stat.Correlation(floatCorpDeletes, floatCommDeletes, nil)
+	insertionsCorrel, deletionsCorrel := corpGroup.Correlation(commGroup)
 
 	return &CorporateReport{
 		CorporateGroup: corpGroup,
--- pkg/statistics/authorgroups/groupdata.go
+++ pkg/statistics/authorgroups/groupdata.go
@@ -1,6 +1,9 @@
 package authorgroups
 
-import "github.com/claucambra/commit-analysis-tool/pkg/common"
+import (
+	"github.com/claucambra/commit-analysis-tool/pkg/common"
+	"gonum.org/v1/gonum/stat"
+)
 
 type GroupData struct {
 	GroupName string
@@ -25,4 +28,37 @@ func NewGroupData(report *DomainGroupsReport, groupName string, groupAuthors int
 	groupData.DeletionsPercent = (float32(groupLineChanges.NumDeletions) / float32(report.TotalChanges.NumDeletions)) * 100
 
 	return groupData
+}
+
+// Returns correlations between insertions and deletions of two groups
+func (group *GroupData) Correlation(groupToCorrelate *GroupData) (float64, float64) {
+	yearsInBothGroups := []int{}
+	for year := range group.YearlyLineChanges {
+		_, ok := groupToCorrelate.YearlyLineChanges[year]
+		if ok {
+			yearsInBothGroups = append(yearsInBothGroups, year)
+		}
+	}
+
+	thisYearlyInsertionsArr, thisYearlyDeletionsArr := group.YearlyLineChanges.SeparatedChangeArrays(yearsInBothGroups)
+	thatYearlyInsertionsArr, thatYearlyDeletionsArr := groupToCorrelate.YearlyLineChanges.SeparatedChangeArrays(yearsInBothGroups)
+
+	floatThisInserts := make([]float64, len(yearsInBothGroups))
+	floatThisDeletes := make([]float64, len(yearsInBothGroups))
+	floatThatInserts := make([]float64, len(yearsInBothGroups))
+	floatThatDeletes := make([]float64, len(yearsInBothGroups))
+
+	for i := 0; i < len(group.YearlyLineChanges); i++ {
+		floatThisInserts[i] = float64(thisYearlyInsertionsArr[i])
+		floatThisDeletes[i] = float64(thisYearlyDeletionsArr[i])
+	}
+	for i := 0; i < len(groupToCorrelate.YearlyLineChanges); i++ {
+		floatThatInserts[i] = float64(thatYearlyInsertionsArr[i])
+		floatThatDeletes[i] = float64(thatYearlyDeletionsArr[i])
+	}
+
+	insertionsCorrel := stat.Correlation(floatThisInserts, floatThatInserts, nil)
+	deletionsCorrel := stat.Correlation(floatThisDeletes, floatThatDeletes, nil)
+
+	return insertionsCorrel, deletionsCorrel
 }
