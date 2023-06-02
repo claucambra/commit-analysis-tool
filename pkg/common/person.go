4b34ba278134f5b8236690768cba89028de18b79
Claudio Cambra <developer@claudiocambra.com>
2023-06-02 20:51:22 +0800

    Add convenience method to get people count in YearlyPeopleMap
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/common/person.go
+++ pkg/common/person.go
@@ -6,3 +6,19 @@ type Person struct {
 }
 
 type YearlyPeopleMap map[int][]*Person
+
+func (ypm *YearlyPeopleMap) CountArray(years []int) []int {
+	yearsToReturn := years
+
+	if yearsToReturn == nil {
+		yearsToReturn = SortedMapKeys(*ypm)
+	}
+
+	countArray := make([]int, len(yearsToReturn))
+
+	for i, year := range yearsToReturn {
+		countArray[i] = len((*ypm)[year])
+	}
+
+	return countArray
+}
