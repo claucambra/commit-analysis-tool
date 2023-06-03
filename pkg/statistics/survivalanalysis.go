ce51e3b7550bd6e98064b063309026b650e3f99b
Claudio Cambra <developer@claudiocambra.com>
2023-06-03 23:35:08 +0800

    Add ability to calculate Kaplan-Meier survival estimate from time-step wise population data
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/survivalanalysis.go
+++ pkg/statistics/survivalanalysis.go
@@ -2,3 +2,27 @@ package statistics
 
 type TimeStepPopulation []int
 type TimeStepSurvival []float64
+
+func (tsp *TimeStepPopulation) KaplanMeierSurvival() TimeStepSurvival {
+	tspLen := len(*tsp)
+	survival := make(TimeStepSurvival, tspLen)
+
+	if tspLen == 0 {
+		return survival
+	}
+
+	prevTimeStepSurvival := 1.0
+	prevTimeStepPopulation := (*tsp)[0]
+
+	for timestep, population := range *tsp {
+		events := prevTimeStepPopulation - population
+		timeStepSurvival := prevTimeStepSurvival * (1 - (float64(events) / float64(prevTimeStepPopulation)))
+
+		survival[timestep] = timeStepSurvival
+
+		prevTimeStepSurvival = timeStepSurvival
+		prevTimeStepPopulation = population
+	}
+
+	return survival
+}
