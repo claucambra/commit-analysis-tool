babf78da307cde6d2d5f65144ec12cf163ade5c2
Claudio Cambra <developer@claudiocambra.com>
2023-06-04 16:52:56 +0800

    Include testing for unknown group data in domaingroupsreport test
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/authorgroups/domaingroupsreport_test.go
+++ pkg/statistics/authorgroups/domaingroupsreport_test.go
@@ -1,11 +1,10 @@
 package authorgroups
 
 import (
-	"reflect"
 	"testing"
 
 	dbtesting "github.com/claucambra/commit-analysis-tool/internal/db/testing"
-	"github.com/claucambra/commit-analysis-tool/pkg/common"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestDomainGroupsReportGroupData(t *testing.T) {
@@ -16,25 +15,20 @@ func TestDomainGroupsReportGroupData(t *testing.T) {
 
 	dbtesting.IngestTestCommits(sqlb, t)
 
-	report := NewDomainGroupsReport(testEmailGroups, sqlb)
+	report := NewDomainGroupsReport(testEmailGroups(), sqlb)
 	report.Generate()
 
-	testGroupData := &GroupData{
-		GroupName: testGroupName,
-		Authors:   testGroupAuthors,
-		LineChanges: &common.LineChanges{
-			NumInsertions: testGroupInsertions,
-			NumDeletions:  testGroupDeletions,
-		},
-		YearlyLineChanges: testGroupYearlyLineChanges,
-		YearlyAuthors:     testGroupYearlyAuthors,
-		AuthorsPercent:    (float32(testNumGroupAuthors) / float32(testNumAuthors)) * 100,
-		InsertionsPercent: (float32(testGroupInsertions) / float32(testNumInsertions)) * 100,
-		DeletionsPercent:  (float32(testGroupDeletions) / float32(testNumDeletions)) * 100,
-	}
+	testGroupData := testGroupData()
 	groupData := report.GroupData(testGroupName)
 
 	if !cmp.Equal(testGroupData, groupData) {
+		t.Fatalf(`Retrieved group data does not match test group data: %s`, cmp.Diff(testGroupData, groupData))
+	}
+
+	testUnknownGroupData := testUnknownGroupData()
+	unknownGroupData := report.GroupData("")
+
+	if !cmp.Equal(testUnknownGroupData, unknownGroupData) {
 		t.Fatalf(`Retrieved group data does not match test group data: %s`, cmp.Diff(testGroupData, groupData))
 	}
 }
