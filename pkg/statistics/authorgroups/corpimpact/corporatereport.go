c9166cb58a06dfe2e2fb8d8d6c6fb49b32e3ee3a
Claudio Cambra <developer@claudiocambra.com>
2023-06-06 02:57:15 +0800

    Add quick CSV line generator in corporate report
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/authorgroups/corpimpact/corporatereport.go
+++ pkg/statistics/authorgroups/corpimpact/corporatereport.go
@@ -1,6 +1,8 @@
 package corpimpact
 
 import (
+	"strconv"
+
 	"github.com/claucambra/commit-analysis-tool/internal/db"
 	"github.com/claucambra/commit-analysis-tool/pkg/statistics/authorgroups"
 	"github.com/claucambra/commit-analysis-tool/pkg/statistics/commitimpact"
@@ -71,4 +73,98 @@ func (corpReport *CorporateReport) Generate() {
 	commGroupImpact := commitimpact.NewCommitImpactReport(commGroup.Commits)
 	commGroupImpact.Generate()
 	corpReport.CommunityCommitImpactReport = commGroupImpact
+}
+
+func (cr *CorporateReport) CSVString(name string, includeHeader bool) [][]string {
+	numSurvValuesToWrite := 8
+	safeCorpSurvivalValues := make([]float64, numSurvValuesToWrite)
+	safeCommSurvivalValues := make([]float64, numSurvValuesToWrite)
+	actualCorpSurvivalValuesLen := len(cr.CorporateGroupSurvivalReport.AuthorsSurvival)
+	actualCommSurvivalValuesLen := len(cr.CommunityGroupSurvivalReport.AuthorsSurvival)
+
+	for i := 0; i < numSurvValuesToWrite-1; i++ {
+		corpSurvivalVal := 0.
+		commSurvivalVal := 0.
+
+		if i > actualCorpSurvivalValuesLen-1 {
+			corpSurvivalVal = cr.CorporateGroupSurvivalReport.AuthorsSurvival[i]
+		}
+
+		if i > actualCommSurvivalValuesLen-1 {
+			commSurvivalVal = cr.CommunityGroupSurvivalReport.AuthorsSurvival[i]
+		}
+
+		safeCorpSurvivalValues[i] = corpSurvivalVal
+		safeCommSurvivalValues[i] = commSurvivalVal
+	}
+
+	csvfiedReport := []string{
+		name,
+		strconv.FormatInt(int64(len(cr.DomainGroupsReport.TotalCommits)), 10),
+		strconv.FormatFloat(cr.InsertionsCorrel, 'E', -1, 64),
+		strconv.FormatFloat(cr.DeletionsCorrel, 'E', -1, 64),
+		strconv.FormatFloat(cr.AuthorsCorrel, 'E', -1, 64),
+		strconv.FormatFloat(cr.CorporateCommitImpactReport.MeanImpact, 'E', -1, 64),
+		strconv.FormatFloat(cr.CommunityCommitImpactReport.MeanImpact, 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[0], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[1], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[2], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[3], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[4], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[5], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[6], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[7], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[8], 'E', -1, 64),
+		strconv.FormatFloat(safeCorpSurvivalValues[9], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[0], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[1], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[2], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[3], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[4], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[5], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[6], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[7], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[8], 'E', -1, 64),
+		strconv.FormatFloat(safeCommSurvivalValues[9], 'E', -1, 64),
+	}
+
+	var finalReport [][]string
+
+	if includeHeader {
+		header := []string{
+			"name",
+			"num_commits",
+			"insertions_correl",
+			"deletions_correl",
+			"authors_correl",
+			"mean_corp_impact",
+			"mean_comm_impact",
+			"corp_surv_0",
+			"corp_surv_1",
+			"corp_surv_2",
+			"corp_surv_3",
+			"corp_surv_4",
+			"corp_surv_5",
+			"corp_surv_6",
+			"corp_surv_7",
+			"corp_surv_8",
+			"corp_surv_9",
+			"comm_surv_0",
+			"comm_surv_1",
+			"comm_surv_2",
+			"comm_surv_3",
+			"comm_surv_4",
+			"comm_surv_5",
+			"comm_surv_6",
+			"comm_surv_7",
+			"comm_surv_8",
+			"comm_surv_9",
+		}
+
+		finalReport = append(finalReport, header)
+	}
+
+	finalReport = append(finalReport, csvfiedReport)
+
+	return finalReport
 }
