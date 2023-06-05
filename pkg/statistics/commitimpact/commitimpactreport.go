c4a610d6aae8e6072f68a2ad9ee75a82bebe495e
Claudio Cambra <developer@claudiocambra.com>
2023-06-05 15:01:53 +0800

    Add weight map for each planned coding category
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/statistics/commitimpact/commitimpactreport.go
+++ pkg/statistics/commitimpact/commitimpactreport.go
@@ -4,6 +4,12 @@ import (
 	"github.com/claucambra/commit-analysis-tool/pkg/common"
 )
 
+const featureKey = "feature"
+const bugfixKey = "bugfix"
+const documentationKey = "documentation"
+const testingKey = "testing"
+const testDataKey = "testdata"
+
 type CommitImpactReport struct {
 	Commits    common.CommitMap
 	Impact     map[string]float64
@@ -14,5 +20,15 @@ func NewCommitImpactReport(commits common.CommitMap) *CommitImpactReport {
 	return &CommitImpactReport{
 		Commits: commits,
 		Impact:  map[string]float64{},
+	}
+}
+
+func codingWeightMap() map[string]float64 {
+	return map[string]float64{
+		featureKey:       1.0,
+		bugfixKey:        0.8,
+		documentationKey: 0.6,
+		testingKey:       0.3,
+		testDataKey:      0.0,
 	}
 }
