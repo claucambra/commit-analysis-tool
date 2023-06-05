776cd133b67216711195584bccefab26eb8b6b00
Claudio Cambra <developer@claudiocambra.com>
2023-06-05 17:00:38 +0800

    Add batch clone and analyse mode for corp tool
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- cmd/main/corpimpacttool.go
+++ cmd/main/corpimpacttool.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/claucambra/commit-analysis-tool/internal/db"
 	"github.com/claucambra/commit-analysis-tool/pkg/logread"
@@ -14,6 +19,8 @@ import (
 
 func main() {
 	var (
+		batchRead            = flag.String("batch-read", "", "path to file of git clone urls to analyse")
+		clonePath            = flag.String("clone-path", "", "path to store cloned repositories in")
 		ingestDbPath         = flag.String("ingest-db-path", "", "path to database file")
 		readDbPath           = flag.String("read-db-path", "", "path to database file")
 		repoPath             = flag.String("repo-path", "", "path to git repository")
@@ -22,7 +29,17 @@ func main() {
 
 	flag.Parse()
 
-	if *ingestDbPath != "" {
+	if *batchRead != "" {
+
+		if *clonePath == "" {
+			log.Fatalf("Received empty clone path, don't know where to store cloned repos")
+		} else if *domainGroupsFilePath == "" {
+			log.Println("WARNING: No valid domain groupings file has been provided")
+		}
+
+		batchCloneAndRead(*batchRead, *clonePath, *domainGroupsFilePath)
+
+	} else if *ingestDbPath != "" {
 
 		if *repoPath == "" {
 			log.Fatalf("Cannot ingest git repository commits to a database file without a path for said file.")
@@ -93,4 +110,58 @@ func printDomainGroups(readDbPath string, domainGroupsFilePath string, sqlb *db.
 	corpReport := corpimpact.NewCorporateReport(groups, sqlb, "Corporate")
 	corpReport.Generate()
 	fmt.Printf("%+v", corpReport)
+}
+
+func batchCloneAndRead(urlsJsonFile string, clonePath string, domainGroupsFilePath string) {
+	urlsJsonBytes, err := os.ReadFile(urlsJsonFile)
+	if err != nil {
+		log.Fatalf("Error opening batch fetch urls JSON file: %s", err)
+	}
+
+	var urls []string
+	err = json.Unmarshal(urlsJsonBytes, &urls)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	for _, url := range urls {
+		log.Printf("About to clone git repository: %s", url)
+
+		splitUrl := strings.Split(url, "/")
+		repoNameDotGit := splitUrl[len(splitUrl)-1]
+		repoName := strings.TrimSuffix(repoNameDotGit, ".git")
+
+		fullClonePath := filepath.Join(clonePath, repoName)
+		ingestDbPath := filepath.Join(clonePath, repoName+".db")
+
+		cmd := exec.Command("git",
+			"clone",
+			"--no-tags",
+			"--filter=blob:none",
+			"--filter=tree:0",
+			url,
+			fullClonePath)
+
+		var stdBuffer bytes.Buffer
+		mw := io.MultiWriter(os.Stdout, &stdBuffer)
+
+		cmd.Stdout = mw
+		cmd.Stderr = mw
+
+		if err := cmd.Run(); err != nil {
+			log.Panic(err)
+		}
+
+		log.Println(stdBuffer.String())
+
+		sqlb := newSql(ingestDbPath)
+
+		log.Printf("Clone of %s now complete. Beginning commit ingest at %s", repoName, ingestDbPath)
+		ingestRepoCommits(ingestDbPath, fullClonePath, sqlb)
+
+		log.Printf("Commit ingest for %s now complete. Beginning corporate impact analysis.", repoName)
+		printDomainGroups(ingestDbPath, domainGroupsFilePath, sqlb)
+
+		sqlb.Close()
+	}
 }
