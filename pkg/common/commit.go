bbcefda00e240f39985a6519632ad6539ae032b8
Claudio Cambra <developer@claudiocambra.com>
2023-06-07 23:41:05 +0800

    Add convenience func to get year range of commits
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/common/commit.go
+++ pkg/common/commit.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"sort"
+	"time"
+)
+
 // Similar to RFC1123Z but without trailing zero on day
 const TimeFormat = "Mon, 2 Jan 2006 15:04:05 -0700"
 
@@ -28,4 +33,37 @@ func (cm *CommitMap) SubtractCommitMap(cmToSubtract CommitMap) {
 	for id := range cmToSubtract {
 		delete(*cm, id)
 	}
+}
+
+func (cm *CommitMap) YearRange(excludeEmpty bool) []int {
+	years := []int{}
+
+	for _, commit := range *cm {
+		commitTime := time.Unix(commit.AuthorTime, 0).UTC()
+		commitYear := commitTime.Year()
+
+		if found, _ := SliceContains(years, commitYear); found {
+			continue
+		}
+
+		years = append(years, commitYear)
+	}
+
+	sort.Slice(years, func(i, j int) bool {
+		return years[i] < years[j]
+	})
+
+	if excludeEmpty || len(years) == 0 {
+		return years
+	}
+
+	firstYear := years[0]
+	lastYear := years[len(years)-1]
+
+	filledYears := []int{}
+	for i := firstYear; i <= lastYear; i++ {
+		filledYears = append(filledYears, i)
+	}
+
+	return filledYears
 }
