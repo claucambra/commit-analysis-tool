c10d756e388c06851198cbac0722a07454baf114
Claudio Cambra <developer@claudiocambra.com>
2023-06-08 02:22:06 +0800

    Add convenience method to flatten yearmonthcount
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/common/count.go
+++ pkg/common/count.go
@@ -1,4 +1,24 @@
 package common
 
+import (
+	"time"
+)
+
 type YearMonthCount map[int]MonthCount
 type MonthCount map[int]int
+
+func (ymc *YearMonthCount) Flatten() []int {
+	values := []int{}
+	years := SortedMapKeys(*ymc)
+
+	for _, year := range years {
+		for month := int(time.January); month <= int(time.December); month++ {
+			monthCount, ok := (*ymc)[year][month]
+			if ok {
+				values = append(values, monthCount)
+			}
+		}
+	}
+
+	return values
+}
